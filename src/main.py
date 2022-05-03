78ff60557f1fbbabc9653634de544666702af35a
LSLaptop <schneider.7@gmx.de>
2022-05-03 11:18:46 +0200

    succession changed

--- src/main.py
+++ src/main.py
@@ -3,7 +3,8 @@ import rasterio
 from rasterio import features
 import numpy
 import os
-import geopandas
+
+# import geopandas
 
 import rioxarray  # for the extension to load
 import xarray
@@ -12,6 +13,7 @@ import xarray
 
 INPUT_PATH = "./input/"
 OUTPUT_PATH = "./output/"
+DIFF_OUTPUT = OUTPUT_PATH + "veg_diff.tif"
 
 NDVI_THRESHOLD = 0.5
 
@@ -23,8 +25,10 @@ def load_tiff(input_path):
     for f in os.listdir(input_path):
         if os.path.isfile(os.path.join(input_path, f)):
             path_file = os.path.join(input_path, f)
-            year = re.search(r"\d+", f).group()
+            year = int(re.search(r"\d+", f).group())
             dict_of_imgs.update({year: rasterio.open(path_file)})
+    # sort images by year
+    dict_of_imgs = dict(sorted(dict_of_imgs.items()))
     return dict_of_imgs
 
 
@@ -70,10 +74,10 @@ def ndvi_mask(threshold, img_dict):
             bandNIR.astype(numpy.float16) + bandRed.astype(numpy.float16)
         )
 
-        # with rasterio.open(
-        #     os.path.join(OUTPUT_PATH, f"ndvi_{key}.tif"), "w", **kwargs
-        # ) as dst:
-        #     dst.write_band(1, ndvi.astype(rasterio.float32))
+        with rasterio.open(
+            os.path.join(OUTPUT_PATH, f"ndvi_{key}.tif"), "w", **kwargs
+        ) as dst:
+            dst.write_band(1, ndvi.astype(rasterio.float32))
 
         max_v = numpy.nanmax(ndvi)
         print(f"{key} max {max_v}")
@@ -91,26 +95,48 @@ def ndvi_mask(threshold, img_dict):
         print("done mask" + key)
 
     img_dict[key].close()
-    calc_diff()
+    # calc_diff()
 
 
-def calc_diff():
-    xds = xarray.open_dataarray(OUTPUT_PATH + "veg_16.tif", masked=True)
-    xds_match = xarray.open_dataarray(OUTPUT_PATH + "veg_17.tif", masked=True)
-    xds_repr_match = xds.rio.reproject_match(xds_match)
-    xds_sum = xds_repr_match - xds_match
-    xds_sum.rio.to_raster(
-        OUTPUT_PATH + "veg_diff.tif", dtype=numpy.int8, compress="lzw"
-    )
+def calc_succession():
+    path_dict = {}
+    for f in os.listdir(INPUT_PATH):
+        if os.path.isfile(os.path.join(INPUT_PATH, f)):
+            path_file = os.path.join(INPUT_PATH, f)
+            year = int(re.search(r"\d+", f).group())
+            path_dict.update({year: path_file})
+    path_dict = dict(sorted(path_dict.items()))
 
-    with rasterio.open(os.path.join(OUTPUT_PATH, "veg_diff.tif"), "r+") as dst:
-        ras = dst.read(1)
-        ras[ras > 0] = 0
-        ras[ras < 0] = 1
-        dst.write_band(1, ras.astype(numpy.int8))
+    i = 0
+    while i < len(path_dict):
+        print(i)
+        if i == 0:
+            xds = xarray.open_dataarray(list(path_dict.values())[0], masked=True)
+            xds_match = xarray.open_dataarray(list(path_dict.values())[1], masked=True)
+            xds_repr_match = xds.rio.reproject_match(xds_match)
+            xds_sum = xds_repr_match - xds_match
+            xds_sum.rio.to_raster(DIFF_OUTPUT, dtype=numpy.int8, compress="lzw")
+            with rasterio.open(DIFF_OUTPUT, "r+") as dst:
+                ras = dst.read(1)
+                ras[ras > 0] = 0
+                ras[ras < 0] = 1
+                dst.write_band(1, ras.astype(numpy.int8))
+        else:
+            xds = xarray.open_dataarray(OUTPUT_PATH, masked=True)
+            xds_match = xarray.open_dataarray(
+                list(path_dict.values())[i + 1], masked=True
+            )
+            xds_repr_match = xds.rio.reproject_match(xds_match)
+            xds_sum = xds_repr_match + xds_match
+            xds_sum.rio.to_raster(DIFF_OUTPUT, dtype=numpy.int8, compress="lzw")
+            with rasterio.open(DIFF_OUTPUT, "r+") as dst:
+                ras = dst.read(1)
+                ras[ras == 2] = 1
+                ras[ras < 2] = 0
+                dst.write_band(1, ras.astype(numpy.int8))
 
 
 if __name__ == "__main__":
-
-    tiffs = load_tiff(INPUT_PATH)
-    ndvi_mask(NDVI_THRESHOLD, tiffs)
+    calc_succession()
+    # tiffs = load_tiff(INPUT_PATH)
+    # ndvi_mask(NDVI_THRESHOLD, tiffs)
